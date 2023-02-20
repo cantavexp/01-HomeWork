d76f503774ce161bf1a8ff1ef3baa2834c63955e
cantavexp <gunnadar@gmail.com>
2023-02-20 14:06:52 -0600

    https://github.com/cantavexp/PasswordGen

---
+++ Mod3/assets/css/js/JS scripte.js
@@ -0,0 +1,39 @@
+// Assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  var password=document.getElementById("password")
+  var chars="0123456789abcdfeghijklmnopqrstuvwxyz!@#$%&*";
+  var passwordLenght=12;
+  var password="";
+
+  var password=document.getElementById("password");
+
+  function genPassword() {
+     var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+     var passwordLength = 12;
+     var password = "";
+  for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+   }
+         document.getElementById("password").value = password;
+  }
+ 
+ function copyPassword() {
+   var copyText = document.getElementById("password");
+   copyText.select();
+   document.execCommand("copy");  
+ }
+  passwordText.value = password;
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
